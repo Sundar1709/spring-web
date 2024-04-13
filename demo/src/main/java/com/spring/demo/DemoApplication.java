ab53d1fc9d29381a57bd4379da40fb1777bdfaf8
sundaram <sundaramk961@gmail.com>
2024-04-13 16:22:08 +0530

    Added Hello World

--- demo/src/main/java/com/spring/demo/DemoApplication.java
+++ demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.spring.demo;
 
+import com.spring.demo.bar.HelloWorld;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			HelloWorld hello =new HelloWorld();
+			hello.getHelloWorld();
+		};
 	}
 
 }
---
+++ demo/src/main/java/com/spring/demo/bar/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.spring.demo.bar;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class HelloWorld {
+    @GetMapping("/hello")
+      public String getHelloWorld(){
+        return "Hello There !";
+    }
+}
